85f217e33f8810b758c4aa52a7d9eeac1c70ed0f
AHX <vladis9484@gmail.com>
2024-01-12 04:08:33 +0300

    Добавьте файлы проекта.

---
+++ Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VerstaTest.Models
+{
+    public class Order
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string SenderCity { get; set; }
+        [Required]
+        public string SenderAddress { get; set;}
+        [Required]
+        public string RecipientCity { get; set; }
+        [Required]
+        public string RecipientAddress { get; set;}
+        [Required]
+        public int Weight { get; set; }
+        [Required]
+        public DateTime DateOfPickup { get; set; }
+    }
+}
